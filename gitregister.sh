4684a874576718ab8f1f5d47c1eb65ee7ab7cc44
ElonMusk2018 <42662533+ElonMusk2018@users.noreply.github.com>
2018-10-25 09:21:59 -0700

    Add files via upload

---
+++ gitregister.sh
@@ -0,0 +1,10 @@
+git config --list;
+git config --global user.username ElonMusk2018;
+git config --global user.name ElonMusk2018;
+git config --global user.user ElonMusk2018;
+git config --global user.email clogbenner@hotmail.com;
+git config --list;
+
+#chmod +x gitregister.sh
+#run as fallows
+#./gitregister.sh
---
+++ ops1.py
@@ -0,0 +1,17 @@
+#ops1.py lb
+"""
++	Addition	x + y
+-	Subtraction		x - y
+*	Multiplication	x * y
+/	Division	x / y
+%	Modulus	x % y
+**	Exponentiation	x ** y
+//	Floor division	x // y
+"""
+Print ("33 + 17", 33 + 17)
+Print ("33 - 17", 33 - 17)
+Print ("33 * 17", 33 * 17)
+Print ("33 / 17", 33 / 17)
+Print ("33 % 17", 33 % 17)
+Print ("33 ** 17", 33 ** 17)
+Print ("33 // 17", 33 // 17)
